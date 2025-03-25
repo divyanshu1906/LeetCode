d90ae8d69cc8febb6cd858db06b50de7cb1c15eb
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-25 17:22:48 +0530

    LC-partition-equal-subset-sum [Runtime: 158 ms; Memory: 16.8 MB]

--- dynamic-programming/partition-equal-subset-sum.cpp
+++ dynamic-programming/partition-equal-subset-sum.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    bool isPossible(int idx, vector<int>&nums, int currSum, int totalSum, vector<vector<int>>&dp){
-        if(idx==nums.size()){
-            int remSum = totalSum - currSum;
-            return remSum == currSum ? true : false;
-        }
-        
-        if(dp[idx][currSum] != -1) return dp[idx][currSum];
-        bool take = isPossible(idx+1, nums, currSum+nums[idx], totalSum, dp);
-        bool notTake = isPossible(idx+1, nums, currSum, totalSum, dp);
-
-        return dp[idx][currSum] = take||notTake;
-    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
         int totalSum = 0;
         for(int i=0; i<n; i++){
             totalSum += nums[i];
         }
-        vector<vector<int>>dp(n, vector<int>(totalSum+1, -1));
-        return isPossible(0, nums, 0, totalSum, dp);
+        if(totalSum % 2 != 0) return false;
+        int target = totalSum/2;
+        vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=target; j++){
+                bool notTake = dp[i-1][j];
+                bool take = false;
+                if(j>=nums[i-1]){
+                    take = dp[i-1][j-nums[i-1]];
+                }
+                dp[i][j] = take||notTake;
+            }
+        }
+        return dp[n][target];
     }
 };
