cc7aadfe679c5dd76bad0094c98d7be672177484
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-26 17:14:00 +0530

    LC-valid-parenthesis-string [Runtime: N/A; Memory: N/A]

--- string/valid-parenthesis-string.cpp
+++ string/valid-parenthesis-string.cpp
@@ -2,14 +2,22 @@ class Solution {
 public:
     bool checkValidString(string s) {
         int n = s.size();
-        vector<int>st(3,0);
+        int low = 0, high =0;
         for(int i=0; i<n; i++){
-            if(s[i]=='(')st[0]++;
-            else if(s[i]=='*')st[1]++;
-            else st[2]++;
+            if(s[i]=='('){
+                low++;
+                high++;
+            }
+            else if(s[i]==')'){
+                low = max(0, low-1);
+                high--;
+            }
+            else{
+                low = max(0, low-1);
+                high++;
+            }
         }
-        if(st[0]==st[2]) return true;
-        else if(st[0]+st[1] == st[2]) return true;
-        return false;
+        if(high<0) return false;
+        return low==0;
     }
 };
