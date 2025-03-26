84dfe286fd3388fe2ccb686568eb398dc9446b21
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-26 16:02:39 +0530

    LC-minimum-operations-to-make-a-uni-value-grid [Runtime: 51 ms; Memory: 90.2 MB]

--- sorting/minimum-operations-to-make-a-uni-value-grid.cpp
+++ sorting/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -4,25 +4,24 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<int>newArr;
-        if(m==1 && n==1) return 0;
+
         for(int i=0; i<m; i++){
-            if(!newArr.empty()){
-                int size = newArr.size()-1;
-                if(abs(newArr[size]-grid[i][0]) % x != 0)return -1;
-            }
-            newArr.push_back(grid[i][0]);
-            for(int j=1; j<n; j++){
-                if(abs(grid[i][j]-grid[i][j-1]) % x != 0) return -1;
+            for(int j=0; j<n; j++){
                 newArr.push_back(grid[i][j]);
             }
         }
+
         sort(newArr.begin(), newArr.end());
+
         int s = newArr.size();
         int target = newArr[s/2];
         int operations = 0;
-        for(int i=0; i<s; i++){
-            operations += (abs(target-newArr[i]))/x;
+
+        for(int &num : newArr){
+            if(num%x != target%x) return -1;
+            operations += abs(target-num)/x;
         }
+
         return operations;
     }
 };
