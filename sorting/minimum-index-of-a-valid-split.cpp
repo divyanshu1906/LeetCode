c65c3f68ef765ab3458b9635e778cb79c1e340db
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-27 11:51:15 +0530

    LC-minimum-index-of-a-valid-split [Runtime: 0 ms; Memory: 90 MB]

--- sorting/minimum-index-of-a-valid-split.cpp
+++ sorting/minimum-index-of-a-valid-split.cpp
@@ -3,21 +3,19 @@ public:
     int minimumIndex(vector<int>& nums) {
         int n = nums.size();
         int maxOccuredElement = nums[0];
-        int count = 1;
+        int counter = 1;
         int idx = 1;
         while(idx<n){
-            if(nums[idx] != maxOccuredElement) count--;
-            else count++;
-            if(count==0) {
+            if(nums[idx] != maxOccuredElement) counter--;
+            else counter++;
+            if(counter==0) {
                 maxOccuredElement = nums[idx];
-                count = 1;
+                counter = 1;
             }
             idx++;
         }
-        int maxCount=0;
-        for(int i=0; i<n; i++){
-            if(nums[i]==maxOccuredElement) maxCount++;
-        }
+
+        int maxCount = count(nums.begin(), nums.end(), maxOccuredElement);
         int half = ceil(n/2.0);
 
         if(maxCount>half){
