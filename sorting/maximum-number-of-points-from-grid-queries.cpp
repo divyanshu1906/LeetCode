aa747c88bb79f44a556b610d0356d4e735b97459
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-28 22:47:19 +0530

    LC-maximum-number-of-points-from-grid-queries [Runtime: N/A; Memory: N/A]

--- sorting/maximum-number-of-points-from-grid-queries.cpp
+++ sorting/maximum-number-of-points-from-grid-queries.cpp
@@ -2,37 +2,67 @@ class Solution {
 public:
     int n,m;
     vector<vector<int>>directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    void dfs(int i, int j, vector<vector<int>>& grid, int &count, vector<vector<bool>>&visited, int query){
-        visited[i][j] = true;
+    void dfs(vector<vector<int>> &grid, int &count, vector<vector<bool>>&visited, int queryValue, vector<pair<int, int>> &visitFrom){
         queue<pair<int, int>>q;
-        q.push({i, j});
+        for(auto &it : visitFrom){
+            q.push(it);
+        }
+
         while(!q.empty()){
-            int i_ = q.front().first;
-            int j_ = q.front().second;
-            q.pop();
-            count++;
-            for(auto &dir : directions){
-                int row = i_ + dir[0];
-                int col = j_ + dir[1];
-                if(row>=0 && row<m && col>=0 && col<n && !visited[row][col] && query>grid[row][col]){
-                    q.push({row, col});
-                    visited[row][col] = true;
+            int size = q.size();
+            vector<pair<int, int>>lastGreaterElement;
+
+            while(size--){
+                int i_ = q.front().first;
+                int j_ = q.front().second;
+                lastGreaterElement.push_back({i_,j_});
+                q.pop();
+
+                if(queryValue > grid[i_][j_]){
+                    if (!visited[i_][j_]) {
+                        visited[i_][j_] = true;
+                        count++;
+                    }
+                    for(auto &dir : directions){
+                        int row = dir[0]+i_;
+                        int col = dir[1]+j_;
+
+                        if(row>=0 && row<m && col>=0 && col<n && !visited[row][col]){
+                            if (grid[row][col] < queryValue) {
+                                q.push({row, col});
+                            } 
+                            else {
+                                lastGreaterElement.push_back({row, col});
+                            }
+                        }
+                    }
                 }
             }
+            visitFrom = lastGreaterElement;
         }
     }
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
         m = grid.size(), n = grid[0].size();
         int k = queries.size();
-        vector<int>ans(k, 0);
+        unordered_map<int, int>mpp;
+        for(int i=0; i<k; i++){
+            mpp[queries[i]] = i;
+        }
+        sort(queries.begin(), queries.end());
+        vector<int>ans(k, 0), finalAns(k,0);
+        vector<pair<int, int>>visitFrom;
+        visitFrom.push_back({0,0});
+        vector<vector<bool>>visited(m, vector<bool>(n, false));
         for(int i=0; i<k; i++){
             int count = 0;
-            if(queries[i]>grid[0][0]){
-                vector<vector<bool>>visited(m, vector<bool>(n, false));
-                dfs(0, 0, grid, count, visited, queries[i]);
-            }
-            ans[i] = count;
+            dfs(grid, count, visited, queries[i], visitFrom);
+            if(i>0)ans[i] = ans[i-1]+count;
+            else ans[i] = count;
         }
-        return ans;
+        for(int i=0; i<k; i++){
+            int idx = mpp[queries[i]];
+            finalAns[idx] = ans[i];
+        }
+        return finalAns;
     }
 };
