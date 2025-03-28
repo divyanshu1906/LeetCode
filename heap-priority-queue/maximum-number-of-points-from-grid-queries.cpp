49d3f3bcdb7d831a5e46471f6efd61b99e2cd5bc
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-28 23:40:35 +0530

    LC-maximum-number-of-points-from-grid-queries [Runtime: 540 ms; Memory: 124.8 MB]

--- heap-priority-queue/maximum-number-of-points-from-grid-queries.cpp
+++ heap-priority-queue/maximum-number-of-points-from-grid-queries.cpp
@@ -1,68 +1,44 @@
 class Solution {
 public:
-    int n,m;
-    vector<vector<int>>directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    void dfs(vector<vector<int>> &grid, int &count, vector<vector<bool>>&visited, int queryValue, vector<pair<int, int>> &visitFrom){
-        queue<pair<int, int>>q;
-        for(auto &it : visitFrom){
-            q.push(it);
+    vector<vector<int>>directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+        int m = grid.size(), n = grid[0].size();
+
+        int Q = queries.size();
+        vector<int>result(Q, 0);
+
+        vector<pair<int, int>>sortedQ;
+        for(int i=0; i<Q; i++){
+            sortedQ.push_back({queries[i], i});
         }
 
-        while(!q.empty()){
-            int size = q.size();
-            vector<pair<int, int>>lastGreaterElement;
+        sort(sortedQ.begin(), sortedQ.end());
 
-            while(size--){
-                int i_ = q.front().first;
-                int j_ = q.front().second;
-                lastGreaterElement.push_back({i_,j_});
-                q.pop();
+        priority_queue<vector<int>, vector<vector<int>>, greater<>>pq;
+        vector<vector<bool>>visited(m, vector<bool>(n, false));
+        int points = 0;
+        pq.push({grid[0][0], 0, 0});
+        visited[0][0]=true;
 
-                if(queryValue > grid[i_][j_]){
-                    if (!visited[i_][j_]) {
+        for(int i=0; i<Q; i++){
+            int queryValue = sortedQ[i].first;
+            int idx = sortedQ[i].second;
+            while(!pq.empty() && pq.top()[0] < queryValue){
+                int i = pq.top()[1];
+                int j = pq.top()[2];
+                pq.pop();
+                points++;
+                for(auto dir : directions){
+                    int i_ = i + dir[0];
+                    int j_ = j + dir[1];
+                    if(i_>=0 && i_<m && j_>=0 && j_<n && !visited[i_][j_]){
+                        pq.push({grid[i_][j_], i_, j_});
                         visited[i_][j_] = true;
-                        count++;
-                    }
-                    for(auto &dir : directions){
-                        int row = dir[0]+i_;
-                        int col = dir[1]+j_;
-
-                        if(row>=0 && row<m && col>=0 && col<n && !visited[row][col]){
-                            if (grid[row][col] < queryValue) {
-                                q.push({row, col});
-                            } 
-                            else {
-                                lastGreaterElement.push_back({row, col});
-                            }
-                        }
                     }
                 }
             }
-            visitFrom = lastGreaterElement;
+            result[idx] = points;
         }
-    }
-    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        m = grid.size(), n = grid[0].size();
-        int k = queries.size();
-        unordered_map<int, int>mpp;
-        for(int i=0; i<k; i++){
-            mpp[queries[i]] = i;
-        }
-        sort(queries.begin(), queries.end());
-        vector<int>ans(k, 0), finalAns(k,0);
-        vector<pair<int, int>>visitFrom;
-        visitFrom.push_back({0,0});
-        vector<vector<bool>>visited(m, vector<bool>(n, false));
-        for(int i=0; i<k; i++){
-            int count = 0;
-            dfs(grid, count, visited, queries[i], visitFrom);
-            if(i>0)ans[i] = ans[i-1]+count;
-            else ans[i] = count;
-        }
-        for(int i=0; i<k; i++){
-            int idx = mpp[queries[i]];
-            finalAns[idx] = ans[i];
-        }
-        return finalAns;
+        return result;
     }
 };
