4e342f369fcb1a0237eef4109c99bb4e4a228420
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-03-30 17:21:50 +0530

    LC-partition-labels [Runtime: N/A; Memory: N/A]

--- string/partition-labels.cpp
+++ string/partition-labels.cpp
@@ -4,13 +4,13 @@ public:
         int n = s.size();
 
         unordered_map<char, int>mpp;
-        for(int i=n-1; i>=0; i--){
+        for(int i=0; i<n; i++){
             mpp[s[i]] = i;
         } 
 
         vector<int>ans;
         int start=0, end=0;
-        
+
         for(int i=1; i<n; i++){
             end = max(end, mpp[s[i]]);
             if(i==end){
