4abf0f4419e8969c87c980693c83d90419a4c6e1
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-04-01 23:27:56 +0530

    LC-solving-questions-with-brainpower [Runtime: 3 ms; Memory: 119.7 MB]

--- dynamic-programming/solving-questions-with-brainpower.cpp
+++ dynamic-programming/solving-questions-with-brainpower.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    long long calculatingPoints(int idx, vector<vector<int>>& questions, vector<long long>&dp){
-        if(idx>=questions.size()) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        long long take = questions[idx][0] + calculatingPoints(idx+questions[idx][1]+1, questions, dp);
-        long long notTake = calculatingPoints(idx+1, questions, dp);
-        return dp[idx] = max(take, notTake);
-    }
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long>dp(n, -1);
-        return calculatingPoints(0, questions, dp);
+        vector<long long>dp(n, 0);
+        for(int i=n-1; i>=0; i--){
+            int idx = i + questions[i][1]+1;
+            if(idx<n){
+                dp[i] = dp[idx] + questions[i][0];
+            }
+            else{
+                dp[i] = questions[i][0];
+            }
+            if(i<n-1){
+                dp[i] = max(dp[i+1], dp[i]);
+            }
+        }
+        return dp[0];
     }
 };
