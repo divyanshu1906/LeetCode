1dda80de3437f8564a55898218cbe1b355ae04e5
Divyanshu Sinha <115927018+divyanshu1906@users.noreply.github.com>
2025-04-02 17:34:12 +0530

    LC-maximum-value-of-an-ordered-triplet-i [Runtime: 0 ms; Memory: 21.6 MB]

--- array/maximum-value-of-an-ordered-triplet-i.cpp
+++ array/maximum-value-of-an-ordered-triplet-i.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-        long long ans = LLONG_MIN;
+        long long ans = 0, maxDiff=0, maxi=0;
+        
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    long long currAns = ((long long)nums[i]-(long long)nums[j])*(long long)nums[k];
-                    ans = max(ans, currAns);
-                }
-            }
+            ans = max(ans, maxDiff*nums[i]);
+            maxDiff = max(maxDiff, maxi-nums[i]);
+            maxi = max(maxi, (long long )nums[i]);
         }
-        return ans>0 ? ans : 0;
+        return ans;
     }
 };
